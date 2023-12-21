0ce3ced28ec8063367836cbe8656b63958d18da4
Claudio9616 <clares9611@gmail.com>
2023-12-21 09:46:32 +0100

    for version 1.5

--- script.js
+++ script.js
@@ -2,18 +2,16 @@
 let result = ''
 // 2
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    result = i
     // 3
     if (i % 15 === 0) {
         result = 'FizzBuzz';
-        console.log(result)
     } else if (i % 3 === 0) {
         result = 'Fizz';
-        console.log(result)
      } else if (i % 5 === 0) {
         result = 'Buzz';
-        console.log(result)
      }
+     console.log(result)
 }
 
 
